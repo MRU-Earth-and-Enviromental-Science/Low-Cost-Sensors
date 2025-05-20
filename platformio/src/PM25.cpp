dac0fe756dac0d6263e8621786f78a0c139813c9
Shivam Walia <shivamwalia2006@gmail.com>
2025-05-20 11:18:56 -0600

    working copy, need to fix directory

--- platformio/src/PM25.cpp
+++ platformio/src/PM25.cpp
@@ -1,22 +1,22 @@
-#include "PM25.h"
+// #include "PM25.h"
 
-int ledPower = 2;
-int samplingTime = 280;
-int measurePin = 35;
-int deltaTime = 40;
-int sleepTime = 9680;
-float voMeasured = 0;
-float calcVoltage = 0;
-float dustDensity = 0;
+// int ledPower = 2;
+// int samplingTime = 280;
+// int measurePin = 35;
+// int deltaTime = 40;
+// int sleepTime = 9680;
+// float voMeasured = 0;
+// float calcVoltage = 0;
+// float dustDensity = 0;
 
-void calibrateSensorPM25()
-{
-    digitalWrite(ledPower, LOW);
-    delayMicroseconds(samplingTime);
-    voMeasured = analogRead(measurePin);
-    delayMicroseconds(deltaTime);
-    digitalWrite(ledPower, HIGH);
-    delayMicroseconds(sleepTime);
-    calcVoltage = voMeasured * (5.0 / 1024.0);
-    dustDensity = 170 * calcVoltage - 0.1;
-}
+// void calibrateSensorPM25()
+// {
+//     digitalWrite(ledPower, LOW);
+//     delayMicroseconds(samplingTime);
+//     voMeasured = analogRead(measurePin);
+//     delayMicroseconds(deltaTime);
+//     digitalWrite(ledPower, HIGH);
+//     delayMicroseconds(sleepTime);
+//     calcVoltage = voMeasured * (5.0 / 1024.0);
+//     dustDensity = 170 * calcVoltage - 0.1;
+// }
--- platformio/src/Temp.cpp
+++ platformio/src/Temp.cpp
@@ -1,19 +1,19 @@
-#include "Temp.h"
+// #include "Temp.h"
 
-DHT dht(DHTPIN, DHTTYPE);
+// DHT dht(DHTPIN, DHTTYPE);
 
-void readTemperatureHumidity(float &temperature, float &humidity)
-{
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
+// void readTemperatureHumidity(float &temperature, float &humidity)
+// {
+//     float h = dht.readHumidity();
+//     float t = dht.readTemperature();
 
-    if (isnan(h) || isnan(t))
-    {
-        Serial.println("DHT read failed, using -1");
-        h = -1.0;
-        t = -1.0;
-    }
+//     if (isnan(h) || isnan(t))
+//     {
+//         Serial.println("DHT read failed, using -1");
+//         h = -1.0;
+//         t = -1.0;
+//     }
 
-    temperature = t;
-    humidity = h;
-}
+//     temperature = t;
+//     humidity = h;
+// }
