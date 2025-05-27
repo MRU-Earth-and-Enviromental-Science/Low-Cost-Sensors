abe7a521efa3d2a00f41d16f924a251e3fa2a84b
Shivam Walia <shivamwalia2006@gmail.com>
2025-05-27 11:26:39 -0600

    screen functions working

--- platformio/src/main.cpp
+++ platformio/src/main.cpp
@@ -1,35 +1,109 @@
 #include <WiFi.h>
 #include <WebServer.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
+#include <Arduino.h>
 
-const char *ssid = "ESP32-AP";
-const char *password = "12345678";
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 
-WebServer server(80);
+Adafruit_SH1106G display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-void handleData()
+void printWrappedText(const String &text, int x, int y)
 {
-  String json = "{\"number\":" + String(random(0, 100)) + "}";
-  server.send(200, "application/json", json);
+  int lineHeight = 8;
+  int charWidth = 6;
+  std::vector<String> words;
+  String word;
+  for (char c : text)
+  {
+    if (c == ' ')
+    {
+      if (word.length() > 0)
+        words.push_back(word);
+      word = "";
+    }
+    else
+    {
+      word += c;
+    }
+  }
+  if (word.length() > 0)
+    words.push_back(word);
+
+  std::vector<String> lines;
+  String currentLine = "";
+  for (const String &w : words)
+  {
+    if ((currentLine.length() + w.length()) * charWidth > SCREEN_WIDTH)
+    {
+      lines.push_back(currentLine);
+      currentLine = w + " ";
+    }
+    else
+    {
+      currentLine += w + " ";
+    }
+  }
+  if (currentLine.length() > 0)
+    lines.push_back(currentLine);
+
+  for (size_t i = 0; i < lines.size(); i++)
+  {
+    String &line = lines[i];
+    int lineWidth = line.length() * charWidth;
+    int lineX = (SCREEN_WIDTH - lineWidth) / 2;
+    int lineY = y + i * lineHeight;
+    display.setCursor(lineX, lineY);
+    display.print(line);
+  }
 }
 
 void setup()
 {
-  Serial.begin(115200);
-  randomSeed(analogRead(0));
+  display.begin();
+  display.clearDisplay();
 
-  WiFi.softAP(ssid, password);
-  delay(1000);
-  IPAddress IP = WiFi.softAPIP();
-  Serial.print("ESP32 AP IP: ");
-  Serial.println(IP);
+  display.fillRect(48, 20, 33, 25, SH110X_WHITE); // Added rectangle
+  display.fillTriangle(64, 60, 80, 45, 48, 45, SH110X_WHITE);
+  display.fillTriangle(64, 20, 48, 20, 48, 8, SH110X_WHITE); // Left triangle
+  display.fillTriangle(64, 20, 80, 20, 80, 8, SH110X_WHITE); // Right triangle
 
-  server.on("/data", handleData);
-  server.begin();
-  Serial.println("Web server started");
+  display.display();
+  delay(6000);
+  display.clearDisplay();
+
+  auto slideText = [](const String &text)
+  {
+    int16_t x1, y1;
+    uint16_t w, h;
+    display.setTextSize(1);
+    display.setTextColor(SH110X_WHITE);
+    display.getTextBounds(text, 0, 0, &x1, &y1, &w, &h);
+
+    int targetX = (SCREEN_WIDTH - w) / 2;
+    int targetY = (SCREEN_HEIGHT - h) / 2;
+
+    for (int i = 0; i <= 10; ++i)
+    {
+      display.clearDisplay();
+      if (i % 2 == 0 || i > 7) // crude dither
+      {
+        printWrappedText(text, targetX, targetY);
+      }
+      display.display();
+      delay(100);
+    }
+
+    delay(1200);
+  };
+
+  slideText("Mount Royal University");
+  slideText("Sensor System Ready");
+
+  display.clearDisplay();
 }
 
 void loop()
 {
-  server.handleClient();
-  yield(); // Give time to WiFi stack
 }
