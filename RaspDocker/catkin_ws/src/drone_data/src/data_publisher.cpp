2607e98c13258646f0992e2b17fcafd6fbd1e6e1
shivam <shivamwalia2006@gmail.com>
2025-06-09 12:00:39 -0600

    why no work

--- RaspDocker/catkin_ws/src/drone_data/src/data_publisher.cpp
+++ RaspDocker/catkin_ws/src/drone_data/src/data_publisher.cpp
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
 
     // Open serial port to ESP32
-    serial_port = open("/dev/serial0", O_RDWR | O_NOCTTY);
+    serial_port = open("/dev/ttyAMA2", O_RDWR | O_NOCTTY);
     if (serial_port < 0)
     {
         perror("Warning: Unable to open serial port. Continuing without serial output.");
