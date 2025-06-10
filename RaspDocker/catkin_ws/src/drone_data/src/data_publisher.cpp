41aff032fcdd54b17ca3f902b1fd855c6c984efe
Shivam Walia <shivamwalia2006@gmail.com>
2025-06-10 13:50:24 -0600

    updated node sending

--- RaspDocker/catkin_ws/src/drone_data/src/data_publisher.cpp
+++ RaspDocker/catkin_ws/src/drone_data/src/data_publisher.cpp
@@ -27,34 +27,33 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "drone_data_node");
     ros::NodeHandle nh;
 
-    // Open serial port to ESP32
     serial_port = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY);
     if (serial_port < 0)
     {
         perror("Warning: Unable to open serial port. Continuing without serial output.");
         serial_port = -1;
     }
-    else
-    {
-        struct termios tty;
-        tcgetattr(serial_port, &tty);
-        cfsetospeed(&tty, B9600);
-        cfsetispeed(&tty, B9600);
-        tty.c_cflag |= (CLOCAL | CREAD);
-        tty.c_cflag &= ~CSIZE;
-        tty.c_cflag |= CS8;         // 8-bit chars
-        tty.c_cflag &= ~PARENB;     // no parity bit
-        tty.c_cflag &= ~CSTOPB;     // 1 stop bit
-        tty.c_cflag &= ~CRTSCTS;    // no hardware flow control
-        tty.c_lflag = 0;            // no signaling chars, no echo, no canonical processing
-        tty.c_oflag = 0;            // no remapping, no delays
-        tty.c_cc[VMIN] = 0;         // read doesn't block
-        tty.c_cc[VTIME] = 10;       // 1 second read timeout
-        tcsetattr(serial_port, TCSANOW, &tty);
-    }
+    // initalizing the port, setting baud rate and other parameters
+    ROS_INFO("Serial port opened successfully.");
 
-    ros::Subscriber sub = nh.subscribe("/dji_sdk/gps_position", 10, gpsCallback);
-    ros::Rate rate(1);  // 1 Hz
+    struct termios tty;
+    tcgetattr(serial_port, &tty);
+    cfsetospeed(&tty, B115200);
+    cfsetispeed(&tty, B115200);
+    tty.c_cflag |= (CLOCAL | CREAD); // Ignore modem control lines and enable receiver
+    tty.c_cflag &= ~CSIZE;           // Clear the current data size setting
+    tty.c_cflag |= CS8;              // 8 data bits
+    tty.c_cflag &= ~PARENB;          // No parity
+    tty.c_cflag &= ~CSTOPB;          // 1 stop bit
+    tty.c_cflag &= ~CRTSCTS;         // No hardware flow control
+    tty.c_lflag = 0;                 // No signaling chars, no echo, no canonical processing
+    tty.c_oflag = 0;                 // No remapping, no delays
+    tty.c_cc[VMIN] = 0;              // No minimum number of characters to read
+    tty.c_cc[VTIME] = 10;            // 1 second timeout for read operations
+    tcsetattr(serial_port, TCSANOW, &tty);
+
+    ros::Subscriber sub = nh.subscribe("/dji_sdk/gps_position", 10, gpsCallback); // Subscribe to GPS position topic
+    ros::Rate rate(1);                                                            // 1 Hz rate
 
     while (ros::ok())
     {
