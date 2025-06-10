7f5deaec8722e797c48204ce8a5e85f291afbabd
shivam <shivamwalia2006@gmail.com>
2025-06-10 11:40:50 -0600

    esp32 is now parsing ros code

--- RaspDocker/catkin_ws/src/drone_data/src/data_publisher.cpp
+++ RaspDocker/catkin_ws/src/drone_data/src/data_publisher.cpp
@@ -7,9 +7,12 @@
 #include <iostream>
 
 int serial_port;
+bool gps_received = false;
 
 void gpsCallback(const sensor_msgs::NavSatFix::ConstPtr &msg)
 {
+    gps_received = true;
+
     char buffer[100];
     snprintf(buffer, sizeof(buffer), "LAT:%.6f LNG:%.6f\n", msg->latitude, msg->longitude);
     if (serial_port != -1)
@@ -25,29 +28,51 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
 
     // Open serial port to ESP32
-    serial_port = open("/dev/ttyAMA2", O_RDWR | O_NOCTTY);
+    serial_port = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY);
     if (serial_port < 0)
     {
         perror("Warning: Unable to open serial port. Continuing without serial output.");
         serial_port = -1;
     }
-
-    struct termios tty;
-    tcgetattr(serial_port, &tty);
-    cfsetospeed(&tty, B9600);
-    cfsetispeed(&tty, B9600);
-    tty.c_cflag |= (CLOCAL | CREAD);
-    tcsetattr(serial_port, TCSANOW, &tty);
+    else
+    {
+        struct termios tty;
+        tcgetattr(serial_port, &tty);
+        cfsetospeed(&tty, B9600);
+        cfsetispeed(&tty, B9600);
+        tty.c_cflag |= (CLOCAL | CREAD);
+        tty.c_cflag &= ~CSIZE;
+        tty.c_cflag |= CS8;         // 8-bit chars
+        tty.c_cflag &= ~PARENB;     // no parity bit
+        tty.c_cflag &= ~CSTOPB;     // 1 stop bit
+        tty.c_cflag &= ~CRTSCTS;    // no hardware flow control
+        tty.c_lflag = 0;            // no signaling chars, no echo, no canonical processing
+        tty.c_oflag = 0;            // no remapping, no delays
+        tty.c_cc[VMIN] = 0;         // read doesn't block
+        tty.c_cc[VTIME] = 10;       // 1 second read timeout
+        tcsetattr(serial_port, TCSANOW, &tty);
+    }
 
     ros::Subscriber sub = nh.subscribe("/dji_sdk/gps_position", 10, gpsCallback);
-    if (!sub)
-    {
-        ROS_ERROR("Failed to subscribe to GPS topic");
-        std::cout << "Failed to subscribe to GPS topic" << std::endl;
-       // sub = -1;
-    }
-    ros::spin();
+    ros::Rate rate(1);  // 1 Hz
 
-    close(serial_port);
+    while (ros::ok())
+    {
+        if (!gps_received && serial_port != -1)
+        {
+            const char *default_msg = "LAT:-1.000000 LNG:-1.000000\n";
+            write(serial_port, default_msg, strlen(default_msg));
+            ROS_WARN("No GPS fix yet. Sending default value.");
+        }
+
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    if (serial_port != -1)
+    {
+        close(serial_port);
+    }
+
     return 0;
 }
---
+++ RaspDocker/openNode.bash
@@ -0,0 +1,5 @@
+cd catkin_ws
+cd src
+cd drone_data
+cd src
+nano data_publisher.cpp
