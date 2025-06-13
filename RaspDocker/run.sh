3a6febac03ac108846ec713f921960b485a5e429
Shivam Walia <shivamwalia2006@gmail.com>
2025-06-13 09:36:43 -0600

    docker-bash

---
+++ RaspDocker/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source /opt/ros/noetic/setup.bash
+source /home/dji/catkin_ws/devel/setup.bash
+
+export ROS_MASTER_URI=http://localhost:11311
+export ROS_HOSTNAME=localhost
+
+roscore &
+ROSCORE_PID=$!
+
+sleep 5
+
+rosrun dji_sdk dji_sdk_node \
+  _app_id:=${APP_ID:-1163880} \
+  _app_key:=${APP_KEY:-559e776a2a67e8e2e0f015842813272eed634a81a0df29cd25d364ea04738303} \
+  _serial_name:=/dev/ttyUSB0 \
+  _baud_rate:=115200 \
+  _core_connection:=true &
+DJI_NODE_PID=$!
+
+sleep 5
+
+rosrun drone_data data_publisher &
+DATA_PUB_PID=$!
+
+wait $DATA_PUB_PID
--- RaspDocker/run_sample.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /home/dji/.bashrc
-
-echo "Running DJI OSDK Linux Sample..."
-cd /home/dji/Onboard-SDK/build/bin
-
-./djiosdk-flightcontrol-sample /home/dji/Onboard-SDK/sample/platform/linux/common/UserConfig.txt
