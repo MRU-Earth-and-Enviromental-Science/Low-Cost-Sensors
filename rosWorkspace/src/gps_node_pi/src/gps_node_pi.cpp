50799dd7e4ecd281a82322ba7755033692511fe3
Shivam Walia <shivamwalia2006@gmail.com>
2025-07-15 10:47:47 -0600

    working i think

--- rosWorkspace/src/gps_node_pi/src/gps_node_pi.cpp
+++ rosWorkspace/src/gps_node_pi/src/gps_node_pi.cpp
@@ -2,16 +2,18 @@
 #include "sensor_msgs/NavSatFix.h"
 #include "std_msgs/UInt8.h"
 
-
-void gpsCallback(const sensor_msgs::NavSatFix::ConstPtr& msg) {
+void gpsCallback(const sensor_msgs::NavSatFix::ConstPtr &msg)
+{
   ROS_INFO("Lat: %f, Lon: %f, Alt: %f", msg->latitude, msg->longitude, msg->altitude);
 }
 
-void gpsHealthCallback(const std_msgs::UInt8::ConstPtr& msg) {
+void gpsHealthCallback(const std_msgs::UInt8::ConstPtr &msg)
+{
   ROS_INFO("GPS Health: %d", msg->data);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   ros::init(argc, argv, "gps_node");
   ros::NodeHandle nh;
 
