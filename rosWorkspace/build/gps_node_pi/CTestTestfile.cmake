2d7e92a1bafb15eb36d428a7318de8dcfc9c57b0
Shivam Walia <141182014+shivam-2507@users.noreply.github.com>
2025-07-15 10:48:19 -0600

    Delete rosWorkspace/build directory

--- rosWorkspace/build/gps_node_pi/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shivamwalia/Low-Cost-Sensors/rosWorkspace/src/gps_node_pi
-# Build directory: /home/shivamwalia/Low-Cost-Sensors/rosWorkspace/build/gps_node_pi
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
